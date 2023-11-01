7055576ab8506c01c976ba1f7a9d2e1524e73f40
Ornella Bonachera <obonachera@undav.edu>
2023-11-01 15:29:10 -0300

    validations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,26 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+
+const initialProd = { name:"RTX450", price:2500, inStorage:42}
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styleUrls: ['./basic-page.component.css']
-})
+}) 
+export class BasicPageComponent implements OnInit{
+  constructor(private fb: FormBuilder){}
 
-export class BasicPageComponent {
-  constructor(private fb: FormBuilder){
-
+  ngOnInit(): void {
+    this.basicPageForm.reset(initialProd);
   }
-
+  
   public basicPageForm: FormGroup = this.fb.group({
     name:['',[Validators.required,Validators.minLength(3)]], // Initial value, validation functions
     price:[0,[Validators.required, Validators.min(0)]],
     inStorage:[0,[Validators.required]]
   })
 
+  isValidField( field:string ){
+    return this.basicPageForm.controls[field].errors && 
+    this.basicPageForm.controls[field].touched
+  }
+
+  getFieldError(field:string): string | null{
+    if( !this.basicPageForm.controls[field] ) return null;
+
+    const errors = this.basicPageForm.controls[field].errors || {}
+    
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Field is required';
+        case 'minlength':
+          return 'Name has to be at least 3 characters long'
+        default:
+          return 'Error'
+      }
+    }
+    return 'aaaaaa'
+  }
   public onSave(){
-    if(this.basicPageForm.invalid)return;
+    if(this.basicPageForm.invalid){
+      this.basicPageForm.markAllAsTouched;
+      return;
+    }
     console.log(this.basicPageForm.value)
+    this.basicPageForm.reset();
   }
 
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
   styleUrls: ['./dynamic-page.component.css']
 })
 export class DynamicPageComponent {
+    constructor(private fb: FormBuilder){}
 
+    public dynamicForm: FormGroup = this.fb.group({
+      name:['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([
+        ['Fortnite', Validators.required],
+        ['Metal Gear', Validators.required]
+      ])
+    })
+
+    get favoriteGamesControl(){
+      return this.dynamicForm.get('favoriteGames') as FormArray;
+    }
+
+    isValidField( field:string ){
+      return this.dynamicForm.controls[field].errors && 
+      this.dynamicForm.controls[field].touched
+    }
+
+    getFieldError(field:string): string | null{
+      if( !this.dynamicForm.controls[field] ) return null;
+  
+      const errors = this.dynamicForm.controls[field].errors || {}
+      
+      for (const key of Object.keys(errors)) {
+        switch(key){
+          case 'required':
+            return 'Field is required';
+          case 'minlength':
+            return 'Name has to be at least 3 characters long'
+          default:
+            return 'Error'
+        }
+      }
+      return 'aaaaaa'
+    }
+
+    onSubmit():void{
+      if(this.dynamicForm.invalid){
+        this.dynamicForm.markAllAsTouched;
+        return;
+      }
+
+      this.dynamicForm.reset()
+    }
 }
